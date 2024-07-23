f316f82e493ba9ec55f72930628fa263a1ffb46e
Amritesh Chandra <103377226+RSPRIMES1234@users.noreply.github.com>
2024-07-23 22:13:41 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -4,144 +4,129 @@ import music21 as m21
 import numpy as np
 import tensorflow.keras as keras
 
+# Constants
 KERN_DATASET_PATH = "deutschl/erk"
 SAVE_DIR = "dataset"
 SINGLE_FILE_DATASET = "file_dataset"
 MAPPING_PATH = "mapping.json"
 SEQUENCE_LENGTH = 64
 
-# durations are expressed in quarter length
+# Durations are expressed in quarter length
 ACCEPTABLE_DURATIONS = [
-    0.25, # 16th note
-    0.5, # 8th note
+    0.25,  # 16th note
+    0.5,   # 8th note
     0.75,
-    1.0, # quarter note
+    1.0,   # quarter note
     1.5,
-    2, # half note
+    2,     # half note
     3,
-    4 # whole note
+    4      # whole note
 ]
 
-
 def load_songs_in_kern(dataset_path):
-    """Loads all kern pieces in dataset using music21.
+    """Loads all kern pieces in the dataset using music21.
 
-    :param dataset_path (str): Path to dataset
-    :return songs (list of m21 streams): List containing all pieces
+    :param dataset_path (str): Path to the dataset.
+    :return songs (list of m21.stream.Stream): List containing all pieces.
     """
     songs = []
 
-    # go through all the files in dataset and load them with music21
+    # Iterate through all files in the dataset and load them with music21
     for path, subdirs, files in os.walk(dataset_path):
         for file in files:
-
-            # consider only kern files
-            if file[-3:] == "krn":
+            if file.endswith(".krn"):  # Consider only kern files
                 song = m21.converter.parse(os.path.join(path, file))
                 songs.append(song)
     return songs
 
-
 def has_acceptable_durations(song, acceptable_durations):
-    """Boolean routine that returns True if piece has all acceptable duration, False otherwise.
+    """Checks if a song has only acceptable durations.
 
-    :param song (m21 stream):
-    :param acceptable_durations (list): List of acceptable duration in quarter length
-    :return (bool):
+    :param song (m21.stream.Stream): The song to check.
+    :param acceptable_durations (list): List of acceptable durations in quarter length.
+    :return (bool): True if all durations are acceptable, False otherwise.
     """
     for note in song.flat.notesAndRests:
         if note.duration.quarterLength not in acceptable_durations:
             return False
     return True
 
-
 def transpose(song):
-    """Transposes song to C maj/A min
+    """Transposes song to C major/A minor.
 
-    :param piece (m21 stream): Piece to transpose
-    :return transposed_song (m21 stream):
+    :param song (m21.stream.Stream): The song to transpose.
+    :return transposed_song (m21.stream.Stream): The transposed song.
     """
-
-    # get key from the song
+    # Get the key from the song
     parts = song.getElementsByClass(m21.stream.Part)
     measures_part0 = parts[0].getElementsByClass(m21.stream.Measure)
     key = measures_part0[0][4]
 
-    # estimate key using music21
+    # Estimate key using music21 if not already defined
     if not isinstance(key, m21.key.Key):
         key = song.analyze("key")
 
-    # get interval for transposition. E.g., Bmaj -> Cmaj
+    # Calculate the interval for transposition
     if key.mode == "major":
         interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("C"))
     elif key.mode == "minor":
         interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("A"))
 
-    # transpose song by calculated interval
-    tranposed_song = song.transpose(interval)
-    return tranposed_song
-
+    # Transpose song by the calculated interval
+    transposed_song = song.transpose(interval)
+    return transposed_song
 
 def encode_song(song, time_step=0.25):
-    """Converts a score into a time-series-like music representation. Each item in the encoded list represents 'min_duration'
-    quarter lengths. The symbols used at each step are: integers for MIDI notes, 'r' for representing a rest, and '_'
-    for representing notes/rests that are carried over into a new time step. Here's a sample encoding:
+    """Converts a score into a time-series-like music representation.
 
-        ["r", "_", "60", "_", "_", "_", "72" "_"]
-
-    :param song (m21 stream): Piece to encode
-    :param time_step (float): Duration of each time step in quarter length
-    :return:
+    :param song (m21.stream.Stream): The song to encode.
+    :param time_step (float): Duration of each time step in quarter length.
+    :return encoded_song (str): Encoded song as a string.
     """
-
     encoded_song = []
 
     for event in song.flat.notesAndRests:
-
-        # handle notes
+        # Handle notes
         if isinstance(event, m21.note.Note):
-            symbol = event.pitch.midi # 60
-        # handle rests
+            symbol = event.pitch.midi
+        # Handle rests
         elif isinstance(event, m21.note.Rest):
             symbol = "r"
 
-        # convert the note/rest into time series notation
+        # Convert the note/rest into time series notation
         steps = int(event.duration.quarterLength / time_step)
         for step in range(steps):
-
-            # if it's the first time we see a note/rest, let's encode it. Otherwise, it means we're carrying the same
-            # symbol in a new time step
             if step == 0:
                 encoded_song.append(symbol)
             else:
                 encoded_song.append("_")
 
-    # cast encoded song to str
+    # Convert encoded song to string
     encoded_song = " ".join(map(str, encoded_song))
-
     return encoded_song
 
-
 def preprocess(dataset_path):
+    """Main preprocessing function to load, filter, transpose, encode, and save songs.
 
-    # load folk songs
+    :param dataset_path (str): Path to the dataset.
+    """
+    # Load folk songs
     print("Loading songs...")
     songs = load_songs_in_kern(dataset_path)
     print(f"Loaded {len(songs)} songs.")
 
     for i, song in enumerate(songs):
-
-        # filter out songs that have non-acceptable durations
+        # Filter out songs with non-acceptable durations
         if not has_acceptable_durations(song, ACCEPTABLE_DURATIONS):
             continue
 
-        # transpose songs to Cmaj/Amin
+        # Transpose songs to Cmaj/Amin
         song = transpose(song)
 
-        # encode songs with music time series representation
+        # Encode songs
         encoded_song = encode_song(song)
 
-        # save songs to text file
+        # Save encoded songs to text files
         save_path = os.path.join(SAVE_DIR, str(i))
         with open(save_path, "w") as fp:
             fp.write(encoded_song)
@@ -149,106 +134,106 @@ def preprocess(dataset_path):
         if i % 10 == 0:
             print(f"Song {i} out of {len(songs)} processed")
 
-
 def load(file_path):
+    """Loads a song from a file.
+
+    :param file_path (str): Path to the file.
+    :return song (str): The loaded song.
+    """
     with open(file_path, "r") as fp:
         song = fp.read()
     return song
 
-
 def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length):
     """Generates a file collating all the encoded songs and adding new piece delimiters.
 
-    :param dataset_path (str): Path to folder containing the encoded songs
-    :param file_dataset_path (str): Path to file for saving songs in single file
-    :param sequence_length (int): # of time steps to be considered for training
-    :return songs (str): String containing all songs in dataset + delimiters
+    :param dataset_path (str): Path to the folder containing the encoded songs.
+    :param file_dataset_path (str): Path to the file for saving songs in a single file.
+    :param sequence_length (int): Number of time steps to be considered for training.
+    :return songs (str): String containing all songs in the dataset with delimiters.
     """
-
     new_song_delimiter = "/ " * sequence_length
     songs = ""
 
-    # load encoded songs and add delimiters
+    # Load encoded songs and add delimiters
     for path, _, files in os.walk(dataset_path):
         for file in files:
             file_path = os.path.join(path, file)
             song = load(file_path)
-            songs = songs + song + " " + new_song_delimiter
+            songs += song + " " + new_song_delimiter
 
-    # remove empty space from last character of string
-    songs = songs[:-1]
+    # Remove the last space from the string
+    songs = songs.rstrip()
 
-    # save string that contains all the dataset
+    # Save the string containing all the dataset
     with open(file_dataset_path, "w") as fp:
         fp.write(songs)
 
     return songs
 
-
 def create_mapping(songs, mapping_path):
-    """Creates a json file that maps the symbols in the song dataset onto integers
+    """Creates a JSON file that maps the symbols in the song dataset onto integers.
 
-    :param songs (str): String with all songs
-    :param mapping_path (str): Path where to save mapping
-    :return:
+    :param songs (str): String with all songs.
+    :param mapping_path (str): Path to save the mapping.
     """
     mappings = {}
 
-    # identify the vocabulary
+    # Identify the vocabulary
     songs = songs.split()
     vocabulary = list(set(songs))
 
-    # create mappings
+    # Create mappings
     for i, symbol in enumerate(vocabulary):
         mappings[symbol] = i
 
-    # save voabulary to a json file
+    # Save vocabulary to a JSON file
     with open(mapping_path, "w") as fp:
         json.dump(mappings, fp, indent=4)
 
-
 def convert_songs_to_int(songs):
+    """Converts song symbols to integers using the mappings.
+
+    :param songs (str): String with all songs.
+    :return int_songs (list): List of integer-encoded songs.
+    """
     int_songs = []
 
-    # load mappings
+    # Load mappings
     with open(MAPPING_PATH, "r") as fp:
         mappings = json.load(fp)
 
-    # transform songs string to list
+    # Transform songs string to list
     songs = songs.split()
 
-    # map songs to int
+    # Map songs to integers
     for symbol in songs:
         int_songs.append(mappings[symbol])
 
     return int_songs
 
-
 def generate_training_sequences(sequence_length):
-    """Create input and output data samples for training. Each sample is a sequence.
+    """Creates input and output data samples for training. Each sample is a sequence.
 
-    :param sequence_length (int): Length of each sequence. With a quantisation at 16th notes, 64 notes equates to 4 bars
-
-    :return inputs (ndarray): Training inputs
-    :return targets (ndarray): Training targets
+    :param sequence_length (int): Length of each sequence.
+    :return inputs (ndarray): Training inputs.
+    :return targets (ndarray): Training targets.
     """
-
-    # load songs and map them to int
+    # Load songs and map them to integers
     songs = load(SINGLE_FILE_DATASET)
     int_songs = convert_songs_to_int(songs)
 
     inputs = []
     targets = []
 
-    # generate the training sequences
+    # Generate the training sequences
     num_sequences = len(int_songs) - sequence_length
     for i in range(num_sequences):
         inputs.append(int_songs[i:i+sequence_length])
         targets.append(int_songs[i+sequence_length])
 
-    # one-hot encode the sequences
+    # One-hot encode the sequences
     vocabulary_size = len(set(int_songs))
-    # inputs size: (# of sequences, sequence length, vocabulary size)
     inputs = keras.utils.to_categorical(inputs, num_classes=vocabulary_size)
     targets = np.array(targets)
 
@@ -256,15 +241,13 @@ def generate_training_sequences(sequence_length):
 
     return inputs, targets
 
-
 def main():
+    """Main function to preprocess the dataset and create training sequences."""
     preprocess(KERN_DATASET_PATH)
     songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
     create_mapping(songs, MAPPING_PATH)
-    #inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
-
+    # Uncomment the following line to generate training sequences
+    # inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
 
 if __name__ == "__main__":
     main()
-
-
