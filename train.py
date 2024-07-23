494c3d7c969caaa6b2afca9288eef3907e42c747
Amritesh Chandra <103377226+RSPRIMES1234@users.noreply.github.com>
2024-07-23 22:16:55 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,95 +1,35 @@
-# import tensorflow.keras as keras
-# from preprocess import generate_training_sequences, SEQUENCE_LENGTH
-
-# OUTPUT_UNITS = 38
-# NUM_UNITS = [256]
-# LOSS = "sparse_categorical_crossentropy"
-# LEARNING_RATE = 0.001
-# EPOCHS = 90
-# BATCH_SIZE = 64
-# SAVE_MODEL_PATH = "model.h5"
-
-
-# def build_model(output_units, num_units, loss, learning_rate):
-#     """Builds and compiles model
-
-#     :param output_units (int): Num output units
-#     :param num_units (list of int): Num of units in hidden layers
-#     :param loss (str): Type of loss function to use
-#     :param learning_rate (float): Learning rate to apply
-
-#     :return model (tf model): Where the magic happens :D
-#     """
-
-#     # create the model architecture
-#     input = keras.layers.Input(shape=(None, output_units))
-#     x = keras.layers.LSTM(num_units[0])(input)
-#     x = keras.layers.Dropout(0.2)(x)
-
-#     output = keras.layers.Dense(output_units, activation="softmax")(x)
-
-#     model = keras.Model(input, output)
-
-#     # compile model
-#     model.compile(loss=loss,
-#                   optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
-#                   metrics=["accuracy"])
-
-#     model.summary()
-
-#     return model
-
-
-# def train(output_units=OUTPUT_UNITS, num_units=NUM_UNITS, loss=LOSS, learning_rate=LEARNING_RATE):
-#     """Train and save TF model.
-
-#     :param output_units (int): Num output units
-#     :param num_units (list of int): Num of units in hidden layers
-#     :param loss (str): Type of loss function to use
-#     :param learning_rate (float): Learning rate to apply
-#     """
-
-#     # generate the training sequences
-#     inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
-
-#     # build the network
-#     model = build_model(output_units, num_units, loss, learning_rate)
-
-#     # train the model
-#     model.fit(inputs, targets, epochs=EPOCHS, batch_size=BATCH_SIZE)
-
-#     # save the model
-#     model.save(SAVE_MODEL_PATH)
-
-
-# if __name__ == "__main__":
-#     train()
-
-
 import tensorflow.keras as keras
-from preprocessing import gen_train_seq,SEQUENCE_LENGTH
+from preprocessing import gen_train_seq, SEQUENCE_LENGTH
 
-OUTPUT_UNITS=40
-LOSS="sparse_categorical_crossentropy"
-LEARNING_RATE=0.001
-NUM_UNITS=[256]
-EPOCHS=80
-BATCH_SIZE=256
-SAVE_MODEL_PATH="model.h5"
+# Constants
+OUTPUT_UNITS = 40
+LOSS = "sparse_categorical_crossentropy"
+LEARNING_RATE = 0.001
+NUM_UNITS = [256]
+EPOCHS = 80
+BATCH_SIZE = 256
+SAVE_MODEL_PATH = "model.h5"
 
-def build_model(output_units,num_units,loss,learning_rate):
+def build_model(output_units, num_units, loss, learning_rate):
+    """Builds and compiles the model.
 
-    #create the model architecture
-    input=keras.layers.Input(shape=(None,output_units))
-    x=keras.layers.LSTM(num_units[0])(input)
-    x=keras.layers.Dropout(0.2)(x)
+    :param output_units (int): Number of output units.
+    :param num_units (list of int): Number of units in hidden layers.
+    :param loss (str): Type of loss function to use.
+    :param learning_rate (float): Learning rate to apply.
 
-    output=keras.layers.Dense(output_units,activation="softmax")(x)
+    :return model (keras.Model): Compiled model.
+    """
 
-    model=keras.Model(input,output)
+    # Create the model architecture
+    input = keras.layers.Input(shape=(None, output_units))
+    x = keras.layers.LSTM(num_units[0])(input)
+    x = keras.layers.Dropout(0.2)(x)
+    output = keras.layers.Dense(output_units, activation="softmax")(x)
 
+    model = keras.Model(input, output)
 
-    #compile model
+    # Compile the model
     model.compile(loss=loss,
                   optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
                   metrics=["accuracy"])
@@ -97,20 +37,26 @@ def build_model(output_units,num_units,loss,learning_rate):
 
     return model
 
-def train(output_units=OUTPUT_UNITS,num_units=NUM_UNITS,loss=LOSS,learning_rate=LEARNING_RATE):
+def train(output_units=OUTPUT_UNITS, num_units=NUM_UNITS, loss=LOSS, learning_rate=LEARNING_RATE):
+    """Trains and saves the TensorFlow model.
 
-    #generate the training sequences
-    inputs,targets=gen_train_seq(SEQUENCE_LENGTH)
+    :param output_units (int): Number of output units.
+    :param num_units (list of int): Number of units in hidden layers.
+    :param loss (str): Type of loss function to use.
+    :param learning_rate (float): Learning rate to apply.
+    """
 
-    #build the network
-    model=build_model(output_units,num_units,loss,learning_rate)
+    # Generate the training sequences
+    inputs, targets = gen_train_seq(SEQUENCE_LENGTH)
 
-    #train the model
-    model.fit(inputs,targets,epochs=EPOCHS,batch_size=BATCH_SIZE)
+    # Build the network
+    model = build_model(output_units, num_units, loss, learning_rate)
 
-    #save the model
+    # Train the model
+    model.fit(inputs, targets, epochs=EPOCHS, batch_size=BATCH_SIZE)
+
+    # Save the model
     model.save(SAVE_MODEL_PATH)
-
 
 if __name__ == "__main__":
     train()
